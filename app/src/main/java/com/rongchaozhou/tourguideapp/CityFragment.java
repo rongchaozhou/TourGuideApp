53b62705a5ddaff3b2e42b7f371f61ddf9a1f38f
Rongchao Zhou <me@rongchaozhou.com>
2018-06-27 15:25:14 +0800

    Use picasso to load the picture int CityFragment

--- app/src/main/java/com/rongchaozhou/tourguideapp/CityFragment.java
+++ app/src/main/java/com/rongchaozhou/tourguideapp/CityFragment.java
@@ -5,12 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class CityFragment extends Fragment {
+
+    String url = "https://github.com/rongchaozhou/PICTURE/blob/master/hangzhou_fragment.png?raw=true";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_city, container, false);
+
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.cityImage);
+        Picasso.get().load(url).placeholder(R.drawable.placeholder).into(imageView);
+
         return rootView;
     }
 }
